fb0a67a26521685344b7083e56a1fe102411a278
bartoszpro <bartoszprotasewicz@gmail.com>
2024-08-27 22:17:40 -0400

    Changed placeholder images

--- app/components/PopularDestinationsGrid.tsx
+++ app/components/PopularDestinationsGrid.tsx
@@ -5,27 +5,31 @@ const PopularDestinationsGrid: React.FC = () => {
     {
       city: "Miami",
       price: "$74",
-      image: "https://placehold.co/600x400/EEE/31343C",
+      image: "https://www.triptipedia.com/tip/img/FenEayqS9.jpg",
     },
     {
       city: "Dallas",
       price: "$122",
-      image: "https://placehold.co/600x400/EEE/31343C",
+      image:
+        "https://www.parker.edu/wp-content/uploads/2022/04/iStock-1286951857.jpg",
     },
     {
       city: "Las Vegas",
       price: "$136",
-      image: "https://placehold.co/600x400/EEE/31343C",
+      image:
+        "https://greennv.com/wp-content/uploads/2021/07/Cannabis-Tourism-in-Las-Vegas.jpeg",
     },
     {
       city: "London",
       price: "$609",
-      image: "https://placehold.co/600x400/EEE/31343C",
+      image:
+        "https://www.planetware.com/wpimages/2023/01/england-london-top-attractions-things-to-do-intro-paragraph-big-ben-thames-bridge.jpg",
     },
     {
-      city: "Dhaka City",
-      price: "$846",
-      image: "https://placehold.co/600x400/EEE/31343C",
+      city: "Sydney",
+      price: "$1,268",
+      image:
+        "https://media2.thrillophilia.com/images/photos/000/110/418/original/1606295412_shutterstock_1025960785.jpg?width=975&height=600",
     },
   ];
 
@@ -33,22 +37,18 @@ const PopularDestinationsGrid: React.FC = () => {
     <div className='max-w-screen-xl mx-auto mt-4 bg-white rounded-2xl'>
       <h2 className='text-black text-lg font-medium'>Popular destinations</h2>
       <p className='text-xs text-gray-600 mb-4'>
-        Fare data may be cached and not reflect real-time pricing
+        Fare data may be cached and not reflect real-time pricing.
       </p>
       <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4'>
         {destinations.map((destination, index) => (
-          <a
-            href='#'
-            key={index}
-            className='block rounded-lg overflow-hidden group'
-          >
+          <a href='#' key={index} className='block overflow-hidden group'>
             <div className='group-hover:underline'>
               <img
                 src={destination.image}
                 alt={destination.city}
-                className='w-full h-48 object-cover group-hover:opacity-80'
+                className='w-full h-48 object-cover group-hover:opacity-80 rounded-lg'
               />
-              <div className='p-2'>
+              <div className='py-2'>
                 <p className='text-black text-lg'>{destination.city}</p>
                 <p className='text-sm text-gray-600'>
                   from {destination.price}
